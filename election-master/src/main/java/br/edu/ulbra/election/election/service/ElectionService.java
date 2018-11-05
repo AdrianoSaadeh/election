d76ebe7ebe3723b87f0c28ba4f042cb4d76c4973
Adriano Saadeh <adrianosaadeh@gmail.com>
2018-11-05 14:12:25 -0200

    CRUD IMPLEMENTADO

--- election-master/src/main/java/br/edu/ulbra/election/election/service/ElectionService.java
+++ election-master/src/main/java/br/edu/ulbra/election/election/service/ElectionService.java
@@ -1,28 +1,113 @@
 package br.edu.ulbra.election.election.service;
 
+import br.edu.ulbra.election.election.exception.GenericOutputException;
+import br.edu.ulbra.election.election.input.v1.ElectionInput;
+import br.edu.ulbra.election.election.model.Election;
 import br.edu.ulbra.election.election.output.v1.ElectionOutput;
+import br.edu.ulbra.election.election.output.v1.GenericOutput;
 import br.edu.ulbra.election.election.repository.ElectionRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ElectionService {
-    private final ElectionRepository electionRepository;
-    private final ModelMapper modelMapper;
+	private final ElectionRepository electionRepository;
+	private final ModelMapper modelMapper;
 
-    @Autowired
-    public ElectionService(ElectionRepository electionRepository, ModelMapper modelMapper) {
-        this.electionRepository = electionRepository;
-        this.modelMapper = modelMapper;
-    }
+	private static final String MESSAGE_INVALID_ID = "Invalid id";
+	private static final String MESSAGE_ELECTION_NOT_FOUND = "Election not found";
 
-    public List<ElectionOutput> getAll(){
-        Type electionOutputListType = new TypeToken<List<ElectionOutput>>(){}.getType();
-        return modelMapper.map(electionRepository.findAll(), electionOutputListType);
-    }
+	@Autowired
+	public ElectionService(ElectionRepository electionRepository, ModelMapper modelMapper) {
+		this.electionRepository = electionRepository;
+		this.modelMapper = modelMapper;
+	}
+
+	public List<ElectionOutput> getAll() {
+		Type electionOutputListType = new TypeToken<List<ElectionOutput>>() {
+		}.getType();
+		return modelMapper.map(electionRepository.findAll(), electionOutputListType);
+	}
+
+	public ElectionOutput create(ElectionInput electionInput) {
+		validateInput(electionInput);
+		Election election = modelMapper.map(electionInput, Election.class);
+		election = electionRepository.save(election);
+		return modelMapper.map(election, ElectionOutput.class);
+	}
+
+	public ElectionOutput getById(Long electionId) {
+		if (electionId == null)
+			throw new GenericOutputException(MESSAGE_INVALID_ID);
+
+		Election election = electionRepository.findById(electionId).orElse(null);
+		if (election == null)
+			throw new GenericOutputException(MESSAGE_ELECTION_NOT_FOUND);
+
+		return modelMapper.map(election, ElectionOutput.class);
+	}
+
+	public ElectionOutput update(Long electionId, ElectionInput electionInput) {
+		if (electionId == null) {
+			throw new GenericOutputException(MESSAGE_INVALID_ID);
+		}
+		validateInput(electionInput);
+
+		Election election = electionRepository.findById(electionId).orElse(null);
+		if (election == null) {
+			throw new GenericOutputException(MESSAGE_ELECTION_NOT_FOUND);
+		}
+
+		election.setYear(electionInput.getYear());
+		election.setStateCode(electionInput.getStateCode());
+		election.setDescription(electionInput.getDescription());
+		election = electionRepository.save(election);
+
+		return modelMapper.map(election, ElectionOutput.class);
+	}
+
+	public GenericOutput delete(Long electionId) {
+		if (electionId == null) {
+			throw new GenericOutputException(MESSAGE_INVALID_ID);
+		}
+
+		Election election = electionRepository.findById(electionId).orElse(null);
+		if (election == null) {
+			throw new GenericOutputException(MESSAGE_ELECTION_NOT_FOUND);
+		}
+
+		electionRepository.delete(election);
+
+		return new GenericOutput("Election deleted");
+	}
+
+	public List<ElectionOutput> getByYear(Integer year) {
+		Type electionOutputListType = new TypeToken<List<ElectionOutput>>() {
+		}.getType();
+		List<Election> electionsOfTheYear = new ArrayList<>();
+
+		for (Election election : electionRepository.findAll()) {
+			if (election.getYear().equals(year))
+				electionsOfTheYear.add(election);
+		}
+
+		return modelMapper.map(electionsOfTheYear, electionOutputListType);
+	}
+
+	private void validateInput(ElectionInput input) {
+		if (input.getYear() != null && input.getYear() < 0)
+			throw new GenericOutputException("Invalid year");
+
+		if (input.getStateCode() != null && input.getStateCode().length() != 2)
+			throw new GenericOutputException("Invalid statecode");
+
+		if (input.getDescription() != null && input.getDescription().isEmpty())
+			throw new GenericOutputException("Should provide a description");
+	}
 
 }
