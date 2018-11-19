fd06b92ed7a00358246af2f2d77897cceb93998a
alexandrebard <39573844+alexandrebard@users.noreply.github.com>
2018-11-19 20:06:51 -0200

    teste remoção

--- bin/main/db/changelog/scripts/002.CREATE_TABLE_VOTE.sql
+++
@@ -1,8 +0,0 @@
-create table vote (
-  id integer identity primary key,
-  election_id integer not null,
-  voter_id integer not null,
-  candidate_id integer,
-  blank_vote boolean not null,
-  null_vote boolean not null
-);
